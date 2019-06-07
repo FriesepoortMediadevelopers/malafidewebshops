393ea92b2a8acdabea4b6df1b6a3cf8a83886aeb
Thijsgh <35255047+Thijsgh@users.noreply.github.com>
2019-06-07 11:38:21 +0200

    Ik update de index met een header en een footer
    made by hendry

--- index.php
+++ index.php
@@ -8,6 +8,27 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Malafidewebshops</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Toevoegen</button></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <h1>Welkom bij de malafidewebshops van ITGO17AO</h1>
+    <footer class="page-footer font-small bg-dark pt-4">
+        <div class="footer-copyright text-center py-3">© 2018 medemogelijk gemaakt door: 
+            <a href="rocfriesepoort.nl">rocfriesepoort.nl</a>
+        </div>
+    </footer>
 </body>
 </html>
