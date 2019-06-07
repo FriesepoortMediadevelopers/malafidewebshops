b68b4c5be20fc31a73334cb206930d55866c3a85
Thijsgh <35255047+Thijsgh@users.noreply.github.com>
2019-06-07 11:48:27 +0200

    fakka ik zet die css erin

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Malafidewebshops</title>
+    <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
@@ -15,9 +16,6 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
           <li class="nav-item">
             <a class="nav-link" href="#"><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Toevoegen</button></a>
           </li>
@@ -25,7 +23,7 @@
       </div>
     </nav>
     <h1>Welkom bij de malafidewebshops van ITGO17AO</h1>
-    <footer class="page-footer font-small bg-dark text-white pt-4">
+    <footer class="page-footer font-small bg-dark text-white">
         <div class="footer-copyright text-center py-3">© 2018 medemogelijk gemaakt door: 
             <a href="rocfriesepoort.nl">rocfriesepoort.nl</a>
         </div>
